0a943edae0e5b2ad37305f3c8bdf57deb57cc12a
Clark McGrew <clark.mcgrew@stonybrook.edu>
2020-12-23 10:36:07 -0500

    Try a new way of calculating the hit time (not terribly successful).

--- app/testHitTiming.cxx
+++ app/testHitTiming.cxx
@@ -28,6 +28,8 @@ void AnalyzeEvent(Cube::Event& event) {
     static TH1F* histAvgHitTiming = NULL;
     static TH1F* histMaxHitTiming = NULL;
     static TH1F* histLateHitTiming = NULL;
+    static TH1F* histWghtHitTiming = NULL;
+
     if (!histHitTiming) {
         std::cout << "Create the histogram" << std::endl;
         histHitTiming = new TH1F("HitTiming","Time Resolution for Default",
@@ -40,11 +42,24 @@ void AnalyzeEvent(Cube::Event& event) {
         histLateHitTiming = new TH1F("LateHitTiming",
                                      "Time Resolution for Avg. Near Last Hit",
                                      80,-20.0,20.0);
+        histWghtHitTiming = new TH1F("wghtHitTiming",
+                                     "Time Resolution for weighting",
+                                    80,-20.0,20.0);
     }
 
     Cube::Handle<Cube::HitSelection> eventHits
         = event.GetHitSelection();
     if (!eventHits) return;
+
+    // Build a map of 2D to 3D hits.
+    std::map<Cube::Handle<Cube::Hit>,std::set<Cube::Handle<Cube::Hit>>> D2ToD3;
+    for (Cube::HitSelection::iterator h = eventHits->begin();
+         h != eventHits->end(); ++h) {
+        for (int i = 0; i<(*h)->GetConstituentCount();++i) {
+            Cube::Handle<Cube::Hit> ch = (*h)->GetConstituent(i);
+            D2ToD3[ch].insert(*h);
+        }
+    }
 
     for (Cube::HitSelection::iterator h = eventHits->begin();
          h != eventHits->end(); ++h) {
@@ -67,10 +82,24 @@ void AnalyzeEvent(Cube::Event& event) {
         avgT = avgT/avgW;
         histMaxHitTiming->Fill(maxT-trueTime-100.0);
         histAvgHitTiming->Fill(avgT-trueTime-100.0);
+
+        /////////////////////////////////////////////////////////////
+        // Calculate the hit time by looking at the latest hit after
+        // correcting for distance.  Since the fiber time usually comes from
+        // the cube hit on the fiber, this tends to make second and third
+        // cubes on the fiber have an early estimated time.  The hit times
+        // have a small early bias.
         double lateT = 0.0;
         double lateW = 0.0;
+        if ((*h)->GetConstituentCount() != 3) {
+            std::cout <<" OH! " << (*h)->GetConstituentCount() << std::endl;
+        }
         for (int i = 0; i<(*h)->GetConstituentCount();++i) {
             Cube::Handle<Cube::Hit> ch = (*h)->GetConstituent(i);
+            if (ch->GetContributorCount() < 1) {
+                std::cout << " Charge " << (ch)->GetCharge() << std::endl;
+                continue;
+            }
             double dd = ((*h)->GetPosition() - ch->GetPosition()).Mag();
             double tt = ch->GetTime() - dd/200.0;
             if (tt < maxT - 2.5) continue;
@@ -79,6 +108,62 @@ void AnalyzeEvent(Cube::Event& event) {
         }
         lateT = lateT/lateW;
         histLateHitTiming->Fill(lateT-trueTime-100.0);
+
+        ////////////////////////////////////////////////////////////
+        // Calculate hit time assuming that all deposites along the fiber are
+        // close to "simultaneous".  This means that the time for the fiber is
+        // corrected to the distance of the closest crossing fiber.  This
+        // tends to make the hit time to be bias a bit late.
+
+        // Find the latest time for the entire cube.
+        maxT = 0.0;
+        double lateFiberT = 1E+8;
+        double lateFiberD = 1E+8;
+        for (int i = 0; i<(*h)->GetConstituentCount();++i) {
+            Cube::Handle<Cube::Hit> ch = (*h)->GetConstituent(i);
+            double minD = 1E+8;
+            double maxD = 0;
+            for (auto hh:  D2ToD3[ch]) {
+                double dd = (hh->GetPosition() - ch->GetPosition()).Mag();
+                if (dd < minD) minD = dd;
+                if (dd > maxD) maxD = dd;
+            }
+            double dd = ((*h)->GetPosition() - ch->GetPosition()).Mag();
+            double tt = ch->GetTime() - minD/200.0;
+            if (maxT < tt) maxT = tt;
+            for (int s = 0; s < ch->GetContributorCount(); ++s) {
+                int seg = ch->GetContributor(s);
+                Cube::Handle<Cube::G4Hit> g4Hit = event.G4Hits[seg];
+                if (!g4Hit) continue;
+                double tf = g4Hit->GetStart().T();
+                if (tf < lateFiberT) {
+                    lateFiberT = tf;
+                    lateFiberD = (g4Hit->GetStart().Vect()
+                                  - ch->GetPosition()).Mag();
+                }
+            }
+        }
+        // Now take the average of times close to the maxT.
+        lateT = 0.0;
+        lateW = 0.0;
+        double lateQ = 0.0;
+        for (int i = 0; i<(*h)->GetConstituentCount();++i) {
+            Cube::Handle<Cube::Hit> ch = (*h)->GetConstituent(i);
+            double minD = 1E+8;
+            double maxD = 0;
+            for (auto hh:  D2ToD3[ch]) {
+                double dd = (hh->GetPosition() - ch->GetPosition()).Mag();
+                if (dd < minD) minD = dd;
+                if (dd > maxD) maxD = dd;
+            }
+            double dd = ((*h)->GetPosition() - ch->GetPosition()).Mag();
+            double tt = ch->GetTime() - minD/200.0;
+            if (tt < maxT - 2.5) continue;
+            lateT += tt*ch->GetCharge();
+            lateW += ch->GetCharge();
+        }
+        lateT = lateT/lateW;
+        histWghtHitTiming->Fill(lateT-trueTime-100.0);
     }
 
 }
