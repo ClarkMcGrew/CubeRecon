b4d5001c8fd4963f8e1b860592477bdcb1f27643
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-02-18 09:24:45 -0500

    Make the ToolG4Hits functions robust for TPC.

--- tools/ToolG4Hits.cxx
+++ tools/ToolG4Hits.cxx
@@ -18,44 +18,82 @@ namespace {
 std::vector<Cube::Handle<Cube::G4Hit>>
 Cube::Tool::HitG4Hits(Cube::Event& event, Cube::Handle<Cube::Hit> hit) {
     // Collect which trajectories added something to this hit
-    std::set<Cube::Handle<Cube::G4Hit>> segXY;
-    std::set<Cube::Handle<Cube::G4Hit>> segXZ;
-    std::set<Cube::Handle<Cube::G4Hit>> segYZ;
-    for (int c = 0; c<hit->GetConstituentCount(); ++c) {
-        Cube::Handle<Cube::Hit> hh = hit->GetConstituent(c);
-        int projection
-            = Cube::Info::IdentifierProjection(hh->GetIdentifier());
-        for (int g = 0; g < hh->GetContributorCount(); ++g) {
-            int seg = hh->GetContributor(g);
-            Cube::Handle<Cube::G4Hit> g4Hit = event.G4Hits[seg];
-            if (!g4Hit) continue;
-            switch (projection) {
-            case Cube::Info::kXYProj: {
-                segXY.insert(g4Hit);
-                break;
-            }
-            case Cube::Info::kXZProj: {
-                segXZ.insert(g4Hit);
-                break;
-            }
-            case Cube::Info::kYZProj: {
-                segYZ.insert(g4Hit);
-                break;
-            }
-            default:
-                throw std::runtime_error("Inconceivable");
+    std::vector<Cube::Handle<Cube::G4Hit>> result;
+    if (Cube::Info::Is3DST(hit->GetIdentifier())
+        && hit->GetConstituentCount() > 0) {
+        std::set<Cube::Handle<Cube::G4Hit>> segXY;
+        std::set<Cube::Handle<Cube::G4Hit>> segXZ;
+        std::set<Cube::Handle<Cube::G4Hit>> segYZ;
+        for (int c = 0; c<hit->GetConstituentCount(); ++c) {
+            Cube::Handle<Cube::Hit> hh = hit->GetConstituent(c);
+            if (!hh) continue;
+            int projection
+                = Cube::Info::IdentifierProjection(hh->GetIdentifier());
+            for (int g = 0; g < hh->GetContributorCount(); ++g) {
+                int seg = hh->GetContributor(g);
+                Cube::Handle<Cube::G4Hit> g4Hit = event.G4Hits[seg];
+                if (!g4Hit) continue;
+                switch (projection) {
+                case Cube::Info::kXYProj: {
+                    segXY.insert(g4Hit);
+                    break;
+                }
+                case Cube::Info::kXZProj: {
+                    segXZ.insert(g4Hit);
+                    break;
+                }
+                case Cube::Info::kYZProj: {
+                    segYZ.insert(g4Hit);
+                    break;
+                }
+                default:
+                    throw std::runtime_error("Inconceivable");
+                }
             }
         }
+        for (std::set<Cube::Handle<Cube::G4Hit>>::iterator xy = segXY.begin();
+             xy != segXY.end(); ++xy) {
+            if (segXZ.find(*xy) == segXZ.end()) continue;
+            if (segYZ.find(*xy) == segYZ.end()) continue;
+            result.push_back(*xy);
+        }
     }
-    std::vector<Cube::Handle<Cube::G4Hit>> result;
-    for (std::set<Cube::Handle<Cube::G4Hit>>::iterator xy = segXY.begin();
-         xy != segXY.end(); ++xy) {
-        if (segXZ.find(*xy) == segXZ.end()) continue;
-        if (segYZ.find(*xy) == segYZ.end()) continue;
-        result.push_back(*xy);
+    else if (Cube::Info::Is3DST(hit->GetIdentifier())
+        && hit->GetConstituentCount() < 1) {
+        for (int g = 0; g < hit->GetContributorCount(); ++g) {
+            int seg = hit->GetContributor(g);
+            Cube::Handle<Cube::G4Hit> g4Hit = event.G4Hits[seg];
+            if (!g4Hit) continue;
+            result.push_back(g4Hit);
+        }
+    }
+    else if (Cube::Info::IsTPC(hit->GetIdentifier())) {
+        if (hit->GetConstituentCount() > 0) {
+            for (int c = 0; c<hit->GetConstituentCount(); ++c) {
+                Cube::Handle<Cube::Hit> hh = hit->GetConstituent(c);
+                std::vector<Cube::Handle<Cube::G4Hit>> tmp(HitG4Hits(event,hh));
+                std::copy(tmp.begin(),tmp.end(),std::back_inserter(result));
+            }
+        }
+        else if (hit->GetContributorCount() > 0) {
+            for (int g = 0; g < hit->GetContributorCount(); ++g) {
+                int seg = hit->GetContributor(g);
+                Cube::Handle<Cube::G4Hit> g4Hit = event.G4Hits[seg];
+                if (!g4Hit) continue;
+                result.push_back(g4Hit);
+            }
+        }
+        else {
+            std::cout << "Inconcievable."
+                      << " Can't get hit segments for this TPC hit"
+                      << std::endl;
+            throw std::runtime_error("TPC Hit is invalid");
+        }
+    }
+    else if (Cube::Info::IsECal(hit->GetIdentifier())) {
+        std::cout << "Can't get hit segments for ECal yet" << std::endl;
     }
     return result;
-
 }
 
 std::vector<Cube::Handle<Cube::G4Hit>>
