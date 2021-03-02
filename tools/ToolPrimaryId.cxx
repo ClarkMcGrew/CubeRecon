dfb5bb3d2fb2d96fb41dfbefda0ec399bfc00544
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-03-02 10:17:00 -0500

    Add function to find primaries from same vertex.

--- tools/ToolPrimaryId.cxx
+++ tools/ToolPrimaryId.cxx
@@ -2,20 +2,24 @@
 
 #include <CubeEvent.hxx>
 #include <CubeG4Hit.hxx>
+#include <CubeUnits.hxx>
 #include <CubeG4Trajectory.hxx>
 
 // Find the primary trajectory id (this is the trajectory that was started
 // by a GEANT4 primary particle).
-int Cube::Tool::PrimaryId(Cube::Event& event, int trajId) {
-    Cube::Handle<Cube::G4Trajectory> traj = event.G4Trajectories[trajId];
+int Cube::Tool::PrimaryId(const Cube::Event& event, int trajId) {
+    Cube::Event::G4TrajectoryContainer::const_iterator
+        t = event.G4Trajectories.find(trajId);
+    if (t == event.G4Trajectories.end()) return -1;
+    Cube::Handle<Cube::G4Trajectory> traj = t->second;
     if (!traj) return -1;
     if (traj->GetParentId() < 0) return traj->GetTrackId();
     return PrimaryId(event,traj->GetParentId());
 }
 
-std::vector<int> Cube::Tool::AllPrimaries(Cube::Event& event) {
+std::vector<int> Cube::Tool::AllPrimaries(const Cube::Event& event) {
     std::vector<int> output;
-    for (Cube::Event::G4TrajectoryContainer::iterator t
+    for (Cube::Event::G4TrajectoryContainer::const_iterator t
              = event.G4Trajectories.begin();
          t != event.G4Trajectories.end(); ++t) {
         if (!t->second) {
@@ -38,6 +42,49 @@ std::vector<int> Cube::Tool::AllPrimaries(Cube::Event& event) {
         // Make sure that the first particle created by a neutron is recorded
         //  NOT IMPLEMENTED... SHOULD THIS BE IMPLEMENTED?
     }
+    return output;
+}
+
+std::vector<int> Cube::Tool::PrimaryVertex(const Cube::Event& event,
+                                           int trajId) {
+    std::vector<int> output;
+
+    // Find the location of the primary associated with the trajectory id.
+    int primId = Cube::Tool::PrimaryId(event,trajId);
+    Cube::Event::G4TrajectoryContainer::const_iterator
+        t = event.G4Trajectories.find(primId);
+    if (t == event.G4Trajectories.end()) return output;
+    if (!t->second) return output;
+    TLorentzVector vertex = t->second->GetInitialPosition();
+
+    // Add all of the primaries coming from vertex to an output vector.
+    for (t = event.G4Trajectories.begin();
+         t != event.G4Trajectories.end(); ++t) {
+        if (!t->second) {
+            // This happens when a non-existant trajectory id is accessed.
+            // That creates a new entry in the map that doesn't have a
+            // trajectory attached.
+            continue;
+        }
+
+        //  Skip the trajectory if it is a secondary particle.
+        if (0 <= t->second->GetParentId()) continue;
+
+        // The actual vertex information is lost to /dev/null, so check if
+        // this came from the same vertex by looking at the separation between
+        // the initial positions.
+        TLorentzVector sep = t->second->GetInitialPosition() - vertex;
+
+        // Separated times so not the same interaction
+        if (std::abs(sep.T()) > 1.0*unit::ns) continue;
+
+        // Separated locations so not the same interaction.
+        if (sep.Vect().Mag() > 1.0*unit::mm) continue;
+
+        // We have a winner.
+        output.push_back(t->second->GetTrackId());
+    }
+
     return output;
 }
 
--- tools/ToolPrimaryId.hxx
+++ tools/ToolPrimaryId.hxx
@@ -8,13 +8,18 @@ namespace Cube {
         /// started by a GEANT4 primary particle).  The trajectories are a
         /// tree of parents with their children.  This looks at the ancestors
         /// to find the originating parent trajectory.
-        int PrimaryId(Cube::Event& event, int trajId);
+        int PrimaryId(const Cube::Event& event, int trajId);
 
         /// Return a vector of the trajectory id values for all of the primary
         /// particles in the event.  This is trying to find the "useful"
         /// primary trajectories, which means it should include the michel
         /// electrons, and the photons from pizero decay.
-        std::vector<int> AllPrimaries(Cube::Event& event);
+        std::vector<int> AllPrimaries(const Cube::Event& event);
+
+        /// Return a vector of trajectory id values for all of the primary
+        /// particles coming from the same interaction as the primary particle
+        /// for the provided trajectory id.
+        std::vector<int> PrimaryVertex(const Cube::Event& event, int trajId);
     }
 }
 #endif
