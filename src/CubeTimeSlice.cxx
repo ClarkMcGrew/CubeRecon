39378a5dc334e3548bc014d83f060f442bb6fe45
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-03-04 08:03:03 -0500

    Time slice each subdetector separately
    This makes sure that a time slice is for the 3DST, ECal or TPC.  The
    barrel and endcap ECal modules are considered one detector.  The top,
    bottom, and downstream TPCs are considered one detector.

--- src/CubeTimeSlice.cxx
+++ src/CubeTimeSlice.cxx
@@ -2,6 +2,7 @@
 #include "CubeClusterManagement.hxx"
 
 #include "CubeUnits.hxx"
+#include "CubeInfo.hxx"
 
 #include <CubeReconCluster.hxx>
 
@@ -13,6 +14,18 @@ namespace {
     struct timeSort {
         bool operator() (const Cube::Handle<Cube::Hit>& lhs,
                          const Cube::Handle<Cube::Hit>& rhs) {
+            // Order the times by detector and then time.
+            int lhsDet = 0;
+            if (Cube::Info::IsTPC(lhs->GetIdentifier())) lhsDet = 1;
+            else if (Cube::Info::IsECal(lhs->GetIdentifier())) lhsDet = 2;
+            else if (Cube::Info::Is3DST(lhs->GetIdentifier())) lhsDet = 3;
+            else lhsDet = 4;
+            int rhsDet = 0;
+            if (Cube::Info::IsTPC(rhs->GetIdentifier())) rhsDet = 1;
+            else if (Cube::Info::IsECal(rhs->GetIdentifier())) rhsDet = 2;
+            else if (Cube::Info::Is3DST(rhs->GetIdentifier())) rhsDet = 3;
+            else rhsDet = 4;
+            if (lhsDet != rhsDet) return lhsDet < rhsDet;
             return lhs->GetTime() < rhs->GetTime();
         }
     };
@@ -55,12 +68,20 @@ Cube::TimeSlice::Process(const Cube::AlgorithmResult& in,
     // Check for any gaps in the hits and build into separate clusters.
     Cube::HitSelection::iterator first = usedHits->begin();
     Cube::HitSelection::iterator last = usedHits->begin();
+    int lastDet = 0;
     while (last != usedHits->end()) {
-        Cube::HitSelection::iterator prev = last;
-        ++last;
+        Cube::HitSelection::iterator prev = last++;
+        int prevDet = lastDet;
+        if (last != usedHits->end()) {
+            if (Cube::Info::IsTPC((*last)->GetIdentifier())) lastDet = 1;
+            else if (Cube::Info::IsECal((*last)->GetIdentifier())) lastDet = 2;
+            else if (Cube::Info::Is3DST((*last)->GetIdentifier())) lastDet = 3;
+            else lastDet = 4;
+        }
         if (last==usedHits->end()
+            || (lastDet != 0 && lastDet != prevDet)
             || ((*last)->GetTime()-(*prev)->GetTime())>fGapCut) {
-            CUBE_LOG(2) << "Time slice with " << last-first << " hits "
+            CUBE_LOG(2) << "Time slice with " << last-first << " `hits "
                         << " from " << (*first)->GetTime()
                         << " to " << (*prev)->GetTime()
                         << std::endl;
@@ -68,7 +89,7 @@ Cube::TimeSlice::Process(const Cube::AlgorithmResult& in,
             // Build a new cluster.
             Cube::Handle<Cube::ReconCluster> timeCluster
                 = Cube::CreateCluster("timeSlice",
-                                         first,last);
+                                      first,last);
             finalObjects->push_back(timeCluster);
 
             // Start a new cluster
